19e559cda873856b96c17152af5f99e8afcfefd8
Andrey <andrey.nevedin@open.ru>
2019-01-27 13:59:40 +0300

    test

--- src/BinarySearchIterativeWay.java
+++ src/BinarySearchIterativeWay.java
@@ -21,7 +21,7 @@ public class BinarySearchIterativeWay {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         int el = 5;
 
