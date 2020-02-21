1061510feb6ec7f5d3c040747ce59d5daae63bfb
Andrey <andrey.nevedin@open.ru>
2020-02-20 21:30:49 -0800

    test

--- src/array/SubarraySumEqualsK.java
+++ src/array/SubarraySumEqualsK.java
@@ -6,8 +6,8 @@ public class SubarraySumEqualsK {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3};
-        int k = 3;
+        int[] arr = {5, 10, 12, 1, 3, 4, 2, 8};
+        int k = 4;
 
         System.out.println(subarraySum(arr, k));
     }
