a65ccf4bcceb1299263248f8271ac0262983260b
Andrey <andrey.nevedin@open.ru>
2020-02-24 23:57:12 -0800

    test

--- src/array/IntervalListIntersections.java
+++ src/array/IntervalListIntersections.java
@@ -5,44 +5,60 @@ import java.util.List;
 
 public class IntervalListIntersections {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[][] a = new int[4][2];
-	}
-
-	static int[][] intervalIntersection(int[][] a, int[][] b) {
-
-		if (a.length == 0 || b.length == 0) {
-			return new int[][];
-		}
-
-		List<int[]> result = new ArrayList<>();
-
-		int i = 0;
-		int j = 0;
-
-		while (i<a.length && j<b.length) {
-			int[] first = a[i];
-			int[] second = b[j];
+        int[][] a = {{0, 2}, {5, 10}, {13, 23}, {24, 25}};
+        int[][] b = {{1, 5}, {8, 12}, {15, 24}, {25, 26}};
 
 
-			int[] intersection = new int[2];
-			intersection[0] = Math.max(first[0], second[0]);
-			intersection[1] = Math.min(first[1], second[1]);
+        int[][] res = intervalIntersection(a, b);
 
-			result.add(intersection);
+        for (int[] re : res) {
+            for (int i : re) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
 
-			i++;
-			j++;
-		}
+    }
 
-		int[][] res = new int[result.size()][2];
+    static int[][] intervalIntersection(int[][] a, int[][] b) {
 
-		for (int k = 0; k < result.size(); k++) {
-			res[k][0] = result.get(k)[0];
-			res[k][1] = result.get(k)[1];
-		}
+        if (a.length == 0 || b.length == 0) {
+            return new int[0][0];
+        }
 
-		return res;
-	}
+        List<int[]> result = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+            int[] first = a[i];
+            int[] second = b[j];
+            int low = Math.max(first[0], second[0]);
+            int high = Math.min(first[1], second[1]);
+
+            if (low <= high) {
+                result.add(new int[]{low, high});
+            }
+
+            if (a[i][1] < b[j][1]) {
+                i++;
+
+            } else {
+                j++;
+
+            }
+        }
+
+        int[][] res = new int[result.size()][2];
+
+        for (int k = 0; k < result.size(); k++) {
+            res[k][0] = result.get(k)[0];
+            res[k][1] = result.get(k)[1];
+        }
+
+        return res;
+    }
 }
