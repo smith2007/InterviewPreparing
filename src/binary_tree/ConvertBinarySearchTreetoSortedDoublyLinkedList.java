559c20e9901f9a6c74f41813255b58eefe9cf936
smith2007 <smith2007.91@gmail.com>
2020-02-28 17:04:20 -0800

    bla bla

--- src/binary_tree/ConvertBinarySearchTreetoSortedDoublyLinkedList.java
+++ src/binary_tree/ConvertBinarySearchTreetoSortedDoublyLinkedList.java
@@ -3,11 +3,43 @@ package binary_tree;
 public class ConvertBinarySearchTreetoSortedDoublyLinkedList {
 
 	public static void main(String[] args) {
-
+		ConvertBinarySearchTreetoSortedDoublyLinkedList ins = new ConvertBinarySearchTreetoSortedDoublyLinkedList();
+		TreeNode treeNode = ins.treeToDoublyList(StringToNodeBuilder.stringToTreeNode("[4,2,5,1,3]"));
+		System.out.println(treeNode);
 	}
 
+	TreeNode newHead = null;
+	TreeNode tail = null;
+
 	public TreeNode treeToDoublyList(TreeNode root) {
-		return null;
+		if (root == null) {
+			return null;
+		}
+		dive(root, null);
+		newHead.left = tail;
+		tail.right = newHead;
+		return newHead;
+	}
+
+	public TreeNode dive(TreeNode curr, TreeNode prev) {
+		if (curr.left != null) {
+			prev = dive(curr.left, prev);
+		}
+		TreeNode currRight = curr.right;
+		curr.left = prev;
+		if (prev!=null){
+			prev.right = curr;
+		}
+
+		if (newHead == null){
+			newHead = curr;
+		}
+		System.out.println(curr.val);
+		if (currRight != null) {
+			return dive(currRight, curr);
+		}
+		tail = curr;
+		return curr;
 	}
 
 
