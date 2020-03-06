7615def9d13f763037df9b2ea073cab3b79b5d2e
smith2007 <smith2007.91@gmail.com>
2020-03-05 17:43:06 -0800

    bla bla

--- src/trie/SearchSuggestionsSystem.java
+++ src/trie/SearchSuggestionsSystem.java
@@ -6,18 +6,27 @@ import java.util.List;
 public class SearchSuggestionsSystem {
 
   public static void main(String[] args) {
-
+    SearchSuggestionsSystem ss = new SearchSuggestionsSystem();
+    String[] arr = {"mobile", "mouse", "moneypot", "monitor", "mousepad"};
+    List<List<String>> mo = ss.suggestedProducts(arr, "mouse");
+    for (List<String> list : mo) {
+      for (String s : list) {
+        System.out.print(s + " ");
+      }
+      System.out.println();
+    }
   }
 
   TrieNode root = new TrieNode("");
 
   public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+    List<List<String>> res = new ArrayList<>();
+
     if (products.length == 0 || searchWord == null || searchWord.isEmpty()) {
       return new ArrayList<>();
     }
 
     for (String product : products) {
-
       TrieNode curr = root;
       for (char letter : product.toCharArray()) {
         int index = letter - 'a';
@@ -29,7 +38,44 @@ public class SearchSuggestionsSystem {
       curr.word = product;
     }
 
-    return null;
+    String collector = "";
+    for (char c : searchWord.toCharArray()) {
+      collector += c;
+      res.add(getLists(products, collector));
+    }
+    return res;
+  }
+
+  private List<String> getLists(String[] products, String searchWord) {
+    TrieNode curr = root;
+    for (char symb : searchWord.toCharArray()) {
+      int index = symb - 'a';
+
+      TrieNode child = curr.children[index];
+      if (child == null) {
+        return new ArrayList<>();
+      }
+      curr = child;
+    }
+
+    List<String> res = new ArrayList<>();
+    dive(res, curr);
+    return res;
+  }
+
+
+  void dive(List<String> res, TrieNode curr) {
+    if (res.size() == 3) {
+      return;
+    }
+    if (curr.word != null && !curr.word.isEmpty()) {
+      res.add(curr.word);
+    }
+    for (int i = 0; i < curr.children.length; i++) {
+      if (curr.children[i] != null) {
+        dive(res, curr.children[i]);
+      }
+    }
   }
 
   static class TrieNode {
