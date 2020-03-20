4ad63bec0ac5d4ee6d6a9a37f02d423db1c71794
Andrey <andrey.nevedin@open.ru>
2020-03-19 23:53:11 -0700

    test

--- src/graph/IsGraphBipartite.java
+++ src/graph/IsGraphBipartite.java
@@ -1,37 +1,65 @@
 package graph;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class IsGraphBipartite {
 
-  public static void main(String[] args) {
 
-  }
+    /**
+     * 245.IsGraphBipartite
+     * <p>
+     * https://leetcode.com/problems/is-graph-bipartite/
+     * <p>
+     * дан НЕ направленный граф - верни true тогда и только тогда когда этот граф двудольный
+     * <p>
+     * Напомним, что граф является двудольным, если мы можем разделить его набор нод на два независимых лагеря A и B так,
+     * чтобы у каждого ребра графа был один узел в A и другой узел в B.
+     * на картинке два множества красные и синие - граф двудольный
+     */
 
-  public boolean isBipartite(int[][] graph) {
-    int len = graph.length;
-    int[] colors = new int[len];
+    public static void main(String[] args) {
 
-    for (int i = 0; i < len; i++) {
-      if (colors[i] != 0) continue;
-      Queue<Integer> queue = new LinkedList<>();
-      queue.offer(i);
-      colors[i] = 1;   // Blue: 1; Red: -1.
-
-      while (!queue.isEmpty()) {
-        int cur = queue.poll();
-        for (int next : graph[cur]) {
-          if (colors[next] == 0) {          // If this node hasn't been colored;
-            colors[next] = -colors[cur];  // Color it with a different color;
-            queue.offer(next);
-          } else if (colors[next] != -colors[cur]) {   // If it is colored and its color is different, return false;
-            return false;
-          }
-        }
-      }
     }
 
-    return true;
-  }
+    /**
+     * будем красить граф через числа
+     * 0: Haven't been colored yet.
+     * 1: Blue.
+     * -1: Red.
+     *
+     * смысл вот в чем
+     * если нода не была еще раскрашена - раскрашиваем ее
+     * далее используем паралельный цвет что бы раскрасить всех ее соседей
+     * если нода была раскрашена, проверь что текущий цвет точно такой же как тот цвет который ты планируешь
+     * использовать для раскрашивания
+     *
+     */
+    public boolean isBipartite(int[][] graph) {
+        int[] colors = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) { //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validColor(int[][] graph, int[] colors, int colorForColoring, int node) {
+        //сразу првоеряем раскрашена ли наша текущая нода
+        //если она раскрашена то есть не равна нулю!!
+        //то надо проверить что она раскрашена тем чем мы хотели бы ее раскрасить
+        if (colors[node] != 0) {
+            return colors[node] == colorForColoring;
+        }
+        //если нода не была еще раскрашена - раскрашиваем ее
+        colors[node] = colorForColoring;
+        for (int next : graph[node]) {
+            //ну и далее берем соседей этой ноды
+            //далее проверяем всех соседей что они раскрашены правильно в праралельный цвет
+            if (!validColor(graph, colors, -1 * colorForColoring, next)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
