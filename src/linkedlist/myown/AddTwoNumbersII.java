38330e9949e2c517524c746518c4dc857b5aaf1f
Andrey <andrey.nevedin@open.ru>
2020-03-21 13:33:41 -0700

    test

--- src/linkedlist/myown/AddTwoNumbersII.java
+++ src/linkedlist/myown/AddTwoNumbersII.java
@@ -18,7 +18,7 @@ public class AddTwoNumbersII {
         ListNode s2 = new ListNode(6);
         s1.next = s2;
         ListNode s3 = new ListNode(4);
-        s3.next = s2;
+        s2.next = s3;
 
         AddTwoNumbersII numbersII = new AddTwoNumbersII();
         ListNode listNode = numbersII.addTwoNumbers(f1, s1);
@@ -30,34 +30,44 @@ public class AddTwoNumbersII {
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
 
+        //накладываем в стеки
         while (l1 != null) {
             s1.push(l1.val);
             l1 = l1.next;
         }
+
+        //первый и второй
         while (l2 != null) {
             s2.push(l2.val);
             l2 = l2.next;
         }
 
-        int sum = 0;
-        ListNode list = new ListNode(0);
+        int sumCollector = 0;//формируем сумм - как коллектора
+
+        ListNode сurrNode = new ListNode(0);
+
+        //раскручиваем цикл на двух стеках до тех пор пока они оба не опустеют
         while (!s1.empty() || !s2.empty()) {
             if (!s1.empty()) {
-                sum += s1.pop();
+                //накидываем в коллектор сумм т.е для числа 7243 состояние стека будет 3427
+                sumCollector += s1.pop();
             }
             if (!s2.empty()) {
-                sum += s2.pop();
+                sumCollector += s2.pop();
             }
-            list.val = sum % 10;
-
-            ListNode head = new ListNode(sum / 10);
-
-            head.next = list;
-            list = head;
-            sum /= 10;
+            //внимание тут делается очень своеобразный трюк - мы строим новый линкед лист как бы пятясь назад
+            //сначал построили ноду currNode, засетили ее в ноль
+            //потом сделали сложение
+            //а затем надстраиваем перед ней еще одну ноду, назовем ее newHeadNode(0)->currNode
+            //и положим туда остаток если надо
+            сurrNode.val = sumCollector % 10;
+            ListNode newHeadNode = new ListNode(sumCollector / 10);
+            newHeadNode.next = сurrNode;
+            сurrNode = newHeadNode;
+            sumCollector /= 10;
         }
 
-        return list.val == 0 ? list.next : list;
+        return сurrNode.val == 0 ? сurrNode.next : сurrNode;
     }
 
 }
