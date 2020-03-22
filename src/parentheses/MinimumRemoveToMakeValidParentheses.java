d31dba8b32bff168d06e81ce8af089f4feb7a9af
Andrey <andrey.nevedin@open.ru>
2020-03-21 22:40:04 -0700

    test

--- src/parentheses/MinimumRemoveToMakeValidParentheses.java
+++ src/parentheses/MinimumRemoveToMakeValidParentheses.java
@@ -1,4 +1,4 @@
-package stack;
+package parentheses;
 
 import java.util.HashSet;
 import java.util.Set;
--- src/parentheses/ValidParentheses.java
+++ src/parentheses/ValidParentheses.java
@@ -1,4 +1,4 @@
-package stack;
+package parentheses;
 
 import java.util.HashMap;
 import java.util.LinkedList;
