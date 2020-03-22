7621e7861c7fd1df1cb7e5125fbb7c7391f4cc91
Andrey <andrey.nevedin@open.ru>
2020-03-22 01:54:44 -0700

    test

---
+++ src/string/SortCharactersByFrequency.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class SortCharactersByFrequency {
+
+    public static void main(String[] args) {
+
+        String tree = frequencySort("raaeaedere");
+
+        System.out.println(frequencySort(tree));
+    }
+
+    static String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry e = pq.poll();
+            for (int i = 0; i < (int) e.getValue(); i++)
+                sb.append(e.getKey());
+        }
+        return sb.toString();
+    }
+}
