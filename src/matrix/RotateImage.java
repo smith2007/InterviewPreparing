1f338bed2cb9ea9ad39e3133e3f39e627b08f67a
Andrey <andrey.nevedin@open.ru>
2020-03-31 20:20:38 -0700

    test

--- src/matrix/RotateImage.java
+++ src/matrix/RotateImage.java
@@ -2,39 +2,40 @@ package matrix;
 
 public class RotateImage {
 
-	public static void main(String[] args) {
-		int[][] matrix = {{1, 2, 3},
-				{4, 5, 6},
-				{7, 8, 9}};
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
 
-		rotate(matrix);
+        rotate(matrix);
 
-	}
+    }
 
-	static void rotate(int[][] matrix) {
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = i; j < matrix[0].length; j++) {
-				int tmp = matrix[j][i];
-				matrix[j][i] = matrix[i][j];
-				matrix[i][j] = tmp;
-			}
-		}
-
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}
-
-		// reverse each row
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length / 2; j++) {
-				int tmp = matrix[i][j];
-				matrix[i][j] = matrix[i][matrix[0].length - j - 1];
-				matrix[i][matrix[0].length - j - 1] = tmp;
-			}
-		}
-	}
-
+    /**
+     * дано как бы изображение размера эн на эн в виде матрицы чисел - задача повернуть его на 90 градусов
+     * <p>
+     * короче сначала транспонируем матрицу - то есть как бы зеркалируем подиагонали а потом делаем своп
+     */
+    static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2 + n % 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int[] tmp = new int[4];
+                int row = i;
+                int col = j;
+                for (int k = 0; k < 4; k++) {
+                    tmp[k] = matrix[row][col];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+                for (int k = 0; k < 4; k++) {
+                    matrix[row][col] = tmp[(k + 3) % 4];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+            }
+        }
+    }
 }
