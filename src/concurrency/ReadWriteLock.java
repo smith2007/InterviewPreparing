0eb7cd081d8eee911db919268696c172e83c5783
smith2007 <smith2007.91@gmail.com>
2020-04-04 01:18:26 -0700

    bla bla

--- src/concurrency/ReadWriteLock.java
+++ src/concurrency/ReadWriteLock.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 public class ReadWriteLock {
 
+  //все потоки для чтения будем класть в мапу
   private Map<Thread, Integer> readingThreads = new HashMap<>();
 
   private int writeAccesses = 0;
   private int writeRequests = 0;
+
+  //так же будем учитывать поток для чтения
+  //потому что только он имеет эксклюзивную блокировку на это
   private Thread writingThread = null;
 
 
@@ -17,22 +21,21 @@ public class ReadWriteLock {
     while (!canGrantReadAccess(callingThread)) {
       wait();
     }
-
-    readingThreads.put(callingThread,
-        (getReadAccessCount(callingThread) + 1));
+    readingThreads.put(callingThread, (getReadAccessCount(callingThread) + 1));
   }
 
+  //проверяем можем ли мы дать акцесс на чтение
   private boolean canGrantReadAccess(Thread callingThread) {
     if (isWriter(callingThread)) {
-      return true;
+      return true; // если он же писатель и просит блокировку на чтение - дай
     }
     if (hasWriter()) {
-      return false;
+      return false; //если кто-то пишет не даем
     }
-    if (isReader(callingThread)) {
+    if (isReader(callingThread)) { // если этот чувак уже имеет блокировку на чтение - пускай снова
       return true;
     }
-    if (hasWriteRequests()) {
+    if (hasWriteRequests()) { // если есть ожидающие потоки на запись, не давай
       return false;
     }
     return true;
@@ -41,22 +44,28 @@ public class ReadWriteLock {
 
   public synchronized void unlockRead() {
     Thread callingThread = Thread.currentThread();
-    if (!isReader(callingThread)) {
+    if (!isReader(callingThread)) { // если он не брал блокировку - разблокировать нечего
       throw new IllegalMonitorStateException("Calling Thread does not" +
           " hold a read lock on this ReadWriteLock ");
     }
+    // смотрим - сколько раз этот чувак просил блокировку на чтение
     int accessCount = getReadAccessCount(callingThread);
     if (accessCount == 1) {
+      //если один раз - надо удалять его из мапы
       readingThreads.remove(callingThread);
     } else {
-      readingThreads.put(callingThread, (accessCount - 1));
+      //если много раз, просто декремент
+      readingThreads.put(callingThread, accessCount - 1);
     }
+    //пошли сигнал всем кто завис на wait()
     notifyAll();
   }
 
   public synchronized void lockWrite() throws InterruptedException {
+    //инкремент счетчика запросов на write
     writeRequests++;
     Thread callingThread = Thread.currentThread();
+    //можно ли давать ему доступ на запись?
     while (!canGrantWriteAccess(callingThread)) {
       wait();
     }
@@ -78,15 +87,19 @@ public class ReadWriteLock {
   }
 
   private boolean canGrantWriteAccess(Thread callingThread) {
+    //если он один читатель, и он один захватил эту блокировку
     if (isOnlyReader(callingThread)) {
       return true;
     }
+    //если есть другие читатели - шлем нахер
     if (hasReaders()) {
       return false;
     }
+    //если никто не занял еще блокировку на запись
     if (writingThread == null) {
       return true;
     }
+    //если он же и есть писатель, он же ее и занял
     if (!isWriter(callingThread)) {
       return false;
     }
