c86fc214ab927169b8f9ebae85175781799bd028
smith2007 <smith2007.91@gmail.com>
2020-04-11 18:15:41 -0700

    bla bla

---
+++ src/string/LongestPalindrome.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.HashMap;
+
+public class LongestPalindrome {
+
+
+  /**
+   * задача решается через мапу, наша задача набивать каунтер как только мы встрелили какой - то
+   * символ дважды
+   */
+  public int longestPalindrome(String s) {
+    HashMap<Character, Integer> hm = new HashMap<>();
+
+    for (char c : s.toCharArray()) {
+      hm.put(c, hm.getOrDefault(c, 0) + 1);
+    }
+
+    int res = 0;
+    boolean oddAvail = false;
+    for (Integer count : hm.values()) {
+      if (count % 2 == 1) {
+        oddAvail = true;
+        res += (count - 1);
+      } else {
+        res += count;
+      }
+
+    }
+    return oddAvail ? (res + 1) : res;
+  }
+
+}
