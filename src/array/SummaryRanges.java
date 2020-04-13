83cfdc58d0d436b9962cca9473a2a9982d32b7d0
smith2007 <smith2007.91@gmail.com>
2020-04-12 21:40:07 -0700

    bla bla

---
+++ src/array/SummaryRanges.java
@@ -0,0 +1,43 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+
+  /**
+   * Given a sorted integer array without duplicates, return the summary of its ranges.
+   *
+   * Example 1:
+   *
+   * Input:  [0,1,2,4,5,7]
+   * Output: ["0->2","4->5","7"]
+   * Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+   * Example 2:
+   *
+   * Input:  [0,2,3,4,6,8,9]
+   * Output: ["0","2->4","6","8->9"]
+   * Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+   */
+
+  public List<String> summaryRanges(int[] nums) {
+    List<String> result = new ArrayList<>();
+    if(nums.length == 0) return result;
+    int start = nums[0];
+    int end = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      if(nums[i] != nums[i - 1] + 1){
+        if(start == end) result.add(String.valueOf(start));
+        else result.add(start + "->" + end);
+        start = nums[i];
+        end = nums[i];
+      }else{
+        end = nums[i];
+      }
+    }
+    if(start == end) result.add(String.valueOf(start));
+    else result.add(start + "->" + end);
+    return result;
+  }
+
+}
