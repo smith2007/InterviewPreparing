452a4fa70846c26f088a3b229adc07a28d0b1f06
smith2007 <smith2007.91@gmail.com>
2020-05-02 17:31:12 -0700

    bla bla

--- src/divide_and_conquer/TheSkylineProblem.java
+++ src/divide_and_conquer/TheSkylineProblem.java
@@ -1,6 +1,7 @@
 package divide_and_conquer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
@@ -11,87 +12,164 @@ public class TheSkylineProblem {
 
     TheSkylineProblem problem = new TheSkylineProblem();
 
-    int[][] matr = {{2,9,10},{3,7,15},{5,12,12},{15,20,10},{19,24,8}};
+    int[][] matr = {{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
     problem.getSkyline(matr);
   }
 
   /**
    *
-   в общем мутная задача, много на нее времени тратить не хочу, в двух словах опишу
-   как я понял - идея в том что мы на набиваем treeMap из наших как бы высот то есть сортированных по порядку что бы
-   набить как бы чейн зданий образующий вот этот вот скайлайн как только добавляет новый как бы столб - а наше здание
-   по факт образовано двумя столбами левым и правым у них есть координаты на оси x и на оси Y
-   надо смотреть набивать tree map с такими высотами и от них как бы плясать
+   divide and conquer задача - решается по принципу мердж сорта
 
-   по этому первый шаг это координаты нашего здания надо трансформировать в двойные координаты столбцов
+   как решается divide and conquer алгоритмы?
+
+   - определим базовые кейсы
+   - разделим задачу на саб таски и будем решать их рекурсивно
+   - мержим саб-таски получая решение всей проблемой
+
+   Алгоритм:
+
+   getSkyline для n зданий:
+
+   частный случай - Если n == 0: вернуть пустой список.
+   частный случай - Если n == 1: вернуть skyline для одного здания (это просто).
+
+   leftSkyline = getSkyline для первой половины  - n / 2 зданий.
+
+   rightSkyline = getSkyline для последних и второй половины - n / 2 зданий.
+
+   Мержим leftSkyline и rightSkyline.
+
    */
 
+  /**
+   * Divide-and-conquer algorithm to solve skyline problem, which is similar with the merge sort
+   * algorithm.
+   *
+   *
+   */
+  //этот метод будем вызывать рекурсивно
   public List<List<Integer>> getSkyline(int[][] buildings) {
-    List<List<Integer>> res = new ArrayList<>();
-    List<int[]> columns = new ArrayList<>();
+    int n = buildings.length;
+    List<List<Integer>> output = new ArrayList<>();
 
-    //у нас на руках получается трио массив из трех элементов
-    // 2 9 10, 2 - левая граница, 9 - правая граница, 10 - верхняя граница
-    //мы складываем в двумерный массив
-    for (int[] building : buildings) {
-      // начальная точка имеет отрицательное значение высоты
-      columns.add(new int[]{building[0], -building[2]}); // этот элемент содержит левую границу и верхнюю границу
-      // конечная точка имеет нормальное значение высоты
-      columns.add(new int[]{building[1], building[2]}); // тут правая граница и верхняя граница
+    // The base cases
+    //частный случай
+    if (n == 0) {
+      return output;
     }
 
-    //сортируем по возрастанию нашей левой границы здания
-    //по итогу получим все начала зданий по возрастанию
-    Collections.sort(columns, (a, b) -> {
-      if (a[0] == b[0]) {
-        return a[1] - b[1];
+    //частный случай
+    if (n == 1) {
+      int xStart = buildings[0][0]; //грани слева
+      int xEnd = buildings[0][1];//справа
+      int y = buildings[0][2];//высота
+
+      //заполняем и возвращаем
+      output.add(new ArrayList<>() {{
+        add(xStart);
+        add(y);
+      }});
+
+      output.add(new ArrayList<>() {{
+        add(xEnd);
+        add(0);
+      }});
+      // output.add(new int[]{xStart, y});
+      // output.add(new int[]{xEnd, 0});
+      return output;
+    }
+
+    //если базового случая нет - надо сплитать
+    // If there is more than one building,
+    // recursively divide the input into two subproblems.
+    List<List<Integer>> leftSkyline = getSkyline(Arrays.copyOfRange(buildings, 0, n / 2));
+    List<List<Integer>> rightSkyline = getSkyline(Arrays.copyOfRange(buildings, n / 2, n));
+
+    // Merge the results of subproblem together.
+    return mergeSkylines(leftSkyline, rightSkyline);
+  }
+
+  /**
+   *
+   * самый ключевой метод по мержу
+   * Merge two skylines together.
+   */
+  public List<List<Integer>> mergeSkylines(List<List<Integer>> left, List<List<Integer>> right) {
+    int nL = left.size(), nR = right.size();
+    int pL = 0, pR = 0;
+    int currY = 0, leftY = 0, rightY = 0;
+    int x, maxY;
+    List<List<Integer>> output = new ArrayList<List<Integer>>();
+
+    // while we're in the region where both skylines are present
+    while ((pL < nL) && (pR < nR)) {
+      List<Integer> pointL = left.get(pL);
+      List<Integer> pointR = right.get(pR);
+      // pick up the smallest x
+      if (pointL.get(0) < pointR.get(0)) {
+        x = pointL.get(0);
+        leftY = pointL.get(1);
+        pL++;
       } else {
-        return a[0] - b[0];
+        x = pointR.get(0);
+        rightY = pointR.get(1);
+        pR++;
       }
-    });
-
-    //используем тримапу что бы хранить всевозможные высоты,
-    //можно и heap использовать, но хип не поддерживает удаление элемента за логарифмическое время
-    //а вот три поддерживает remove, add и getMax за lgN время
-    //итого
-    //ключ: высоты, value: кол-во таких высот
-    TreeMap<Integer, Integer> heightToCountMap = new TreeMap<>();
-    heightToCountMap.put(0, 1);
-    // до старта предыдущая макс высота как бы полагаем что она равна 0
-    int prev = 0;
-
-    // посещаем все точки по порядку
-    // суть этого цикла в следущем - берем prev и current
-    for (int[] column : columns) {
-      // стартовая как бы точка - добавляем ВЫСОТЫ каждой колонки
-      //далее смотрим каждую высоту в отдельности
-      //массив column[0] - точка на оси Х, column[1] - точка на оси Y - она же как бы высота колонки (может быть инвертированной)
-      //если она инвертированная
-      if (column[1] < 0) {
-        //то кладем ее в мапу каунтеров, предварительно
-        heightToCountMap.merge(-column[1], 1, Integer::sum);
-      } else {  // конечная точка - удаляем высоту
-        if (heightToCountMap.get(column[1]) > 1) {
-          heightToCountMap.put(column[1], heightToCountMap.get(column[1]) - 1);
-        } else {
-          heightToCountMap.remove(column[1]);
-        }
-      }
-
-      //начинаем последней высоты
-      int cur = heightToCountMap.lastKey();
-
-      // сравниваем текущую max height с предыдущей max height,
-      // обновляем массив res и предыдущий max height если необходимо
-      if (cur != prev) {
-        List<Integer> point = new ArrayList<>();
-        point.add(column[0]);
-        point.add(cur);
-        res.add(point);
-        prev = cur;
+      // max height (i.e. y) between both skylines
+      maxY = Math.max(leftY, rightY);
+      // update output if there is a skyline change
+      if (currY != maxY) {
+        updateOutput(output, x, maxY);
+        currY = maxY;
       }
     }
-    return res;
+
+    // there is only left skyline
+    appendSkyline(output, left, pL, nL, currY);
+
+    // there is only right skyline
+    appendSkyline(output, right, pR, nR, currY);
+
+    return output;
+  }
+
+  /**
+   * Update the final output with the new element.
+   */
+  public void updateOutput(List<List<Integer>> output, int x, int y) {
+    // if skyline change is not vertical -
+    // add the new point
+    if (output.isEmpty() || output.get(output.size() - 1).get(0) != x) {
+      output.add(new ArrayList<Integer>() {{
+        add(x);
+        add(y);
+      }});
+    }
+    // if skyline change is vertical -
+    // update the last point
+    else {
+      output.get(output.size() - 1).set(1, y);
+    }
+  }
+
+  /**
+   * Append the rest of the skyline elements with indice (p, n) to the final output.
+   */
+  public void appendSkyline(List<List<Integer>> output, List<List<Integer>> skyline,
+      int p, int n, int currY) {
+    while (p < n) {
+      List<Integer> point = skyline.get(p);
+      int x = point.get(0);
+      int y = point.get(1);
+      p++;
+
+      // update output
+      // if there is a skyline change
+      if (currY != y) {
+        updateOutput(output, x, y);
+        currY = y;
+      }
+    }
   }
 
 }
