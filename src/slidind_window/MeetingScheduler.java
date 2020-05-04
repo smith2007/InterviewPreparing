494d08fb17edaf3432670ba45532b71e4a7ff8ac
smith2007 <smith2007.91@gmail.com>
2020-05-03 18:23:08 -0700

    bla bla

--- src/slidind_window/MeetingScheduler.java
+++ src/slidind_window/MeetingScheduler.java
@@ -9,11 +9,12 @@ import java.util.List;
 public class MeetingScheduler {
 
   /**
-   * по началу надо отсортировать оба по времени начала - ну а затем тут два указателя
-   * - смотрим что больше что меньше и ищем подходящий слот
+   * по началу надо отсортировать оба по времени начала - ну а затем тут два указателя - смотрим что
+   * больше что меньше и ищем подходящий слот
    */
   public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-    if (slots1 == null || slots1.length == 0 || slots2 == null || slots2.length == 0 || duration < 0){
+    if (slots1 == null || slots1.length == 0 || slots2 == null || slots2.length == 0
+        || duration < 0) {
       return Collections.emptyList();
     }
 
@@ -23,23 +24,25 @@ public class MeetingScheduler {
     int len1 = slots1.length;
     int len2 = slots2.length;
 
+    //два указателя
     int i = 0, j = 0;
+
     while (i < len1 && j < len2) {
       int[] s1 = slots1[i];
       int[] s2 = slots2[j];
 
       // Find intersection of their availability slots
-
       int earliestStart = Math.max(s1[0], s2[0]);
       int earliestEnd = Math.min(s1[1], s2[1]);
 
-      if (earliestStart + duration <= earliestEnd)
+      if (earliestStart + duration <= earliestEnd) {
         return new ArrayList<>(Arrays.asList(earliestStart, earliestStart + duration));
-
-      else {
-        if (s1[1] <= s2[1]) i++;
-        else
+      } else {
+        if (s1[1] <= s2[1]) {
+          i++;
+        } else {
           j++;
+        }
       }
     }
     return Collections.emptyList();
