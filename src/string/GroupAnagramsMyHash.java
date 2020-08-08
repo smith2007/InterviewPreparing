588aff4ab249b073c881ebccec99cc1dac87db40
Andrey <andrey.nevedin@open.ru>
2020-08-07 17:32:33 -0700

    test

--- src/string/GroupAnagramsMyHash.java
+++ src/string/GroupAnagramsMyHash.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class GroupAnagramsSorting {
+public class GroupAnagramsMyHash {
 
     public static void main(String[] args) {
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
